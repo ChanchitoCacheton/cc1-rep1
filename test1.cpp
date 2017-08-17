553670e71407b7a22a578a71a1652a2d7de3b071
Uboa <uboa@MacBook-Pro-de-USER.local>
2017-08-17 10:51:25 -0500

    segundo commit

---
+++ test1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	
+	int x,y;
+	
+	cout<<" inserta numero A :"<<endl;
+	cin>>x;
+	
+	cout<<" inserta numero B :"<<endl;
+	cin>>y;
+	
+	int suma;
+	
+	suma= x+y;
+	
+	
+	cout<<" el resultado es :"<<suma<<endl;
+	
+	return 0;
+}
