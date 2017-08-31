8b487b59ce5e91889adc71c9ed520bf67f7e8f1e
Uboa <uboa@MacBook-Pro-de-USER.local>
2017-08-31 11:30:21 -0500

    ej2 semana3

---
+++ sem3_ej2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int n,notas;
+	int mayor=0;
+	int menor=0;
+	float promedio=0;
+   	int suma=0;
+	cout<<"ingrese cantidad de elementos"<<endl;
+	cin>>n;
+
+
+	for(int i=0;i<n;i++){
+
+		cout<<"ingrese un numero : "<<endl;
+		cin>>notas;
+
+		suma=suma+notas;
+
+		if(i==0){
+
+			mayor=notas;
+			menor=notas;
+
+		}
+			else{
+
+				mayor= notas>mayor ? notas : mayor;
+				menor= notas<menor ? notas : menor;
+
+			}
+
+
+
+
+	}
+
+	promedio=suma/n;
+
+	cout<<" promedio : "<<promedio<<endl;
+	cout<<" menor > "<<menor<<endl;
+	cout<<" mayor >"<<mayor<<endl;
+
+
+
+	return 0;
+			
+
+
+
+}
+
+
+
+
+
