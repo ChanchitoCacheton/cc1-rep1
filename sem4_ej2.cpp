e83cc6c31a191430a4c3d9b97f224f1f8ccfa546
Uboa <uboa@MacBook-Pro-de-USER.local>
2017-09-05 09:32:48 -0500

    ej2 semana 4

---
+++ sem4_ej2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n;
+
+	cout<<"ingrese n: "<<endl;
+	cin>>n;
+
+	do{ 
+		cout<<"hello world"<<endl;
+		}while(--n>0);
+
+		
+	return 0;
+}
