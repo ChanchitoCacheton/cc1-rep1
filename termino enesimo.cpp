e85c59bde07c8c6ad7042c21cb92bbdd21b4e862
Uboa <uboa@MacBook-Pro-de-USER.local>
2017-09-07 10:46:43 -0500

    termino enesimo v2

--- termino enesimo.cpp
+++ termino enesimo.cpp
@@ -1,22 +1,34 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-  int num,n,p,i;
-  cout<<"Ingresa un numero"<<endl;
-  cin>>num;
+
+
+void busquedaPrimo(int num){
+
+  int n,p,i;
   n=2;
   i=0;
+
   while (i<=num)
   { for (p=2;n%p!=0;p++);
     if(p==n)
       {i=i+1;
         if(i==num)
           
-          cout<<n<<endl;
+          cout<<"el numero primo en esa posicion es :"<<n<<endl;
       }
     n=n+1;
   }
+
+
+}
+
+int main()
+{
+  int num,n,p,i;
+  cout<<"Ingresa un numero"<<endl;
+  cin>>num;
+  
+  busquedaPrimo(num);
   return 0;
 }
