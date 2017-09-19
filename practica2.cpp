a8dcd997b610c8f740723ff8aecf0b0887e97513
Uboa <uboa@MacBook-Pro-de-USER.local>
2017-09-19 09:52:50 -0500

    ej2 practica v2

--- practica2.cpp
+++ practica2.cpp
@@ -3,20 +3,22 @@
 using namespace std;
 
 int calculaSum(int arr[],int n){
-	int sum=0;
-	for(int i=0;i<n;i++)
-	{
-		sum+=arr[i];	
-	}	
+	//int sum;
 
-	return sum;
+	if(n==1){
+		return arr[0];
+	}
+	else
+		return arr[n-1] + calculaSum(arr,n-1);
+		
 }
 int main(){
 
 	int n=5;
 	int arr[]={1,2,3,4,5};
 
-	cout<<calculaSum(arr,n);
+	cout<<endl;
+	cout<<"la suma de los elementos del arreglo: "<<calculaSum(arr,n)<<endl;
 
 
 
